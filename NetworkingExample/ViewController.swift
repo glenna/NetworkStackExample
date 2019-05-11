c075d0b71290d468e6bb9a135e205b186ff6c4a8
Glenna Buford <1899476+glenna@users.noreply.github.com>
2019-05-11 18:03:28 +0200

    Initial Commit

---
+++ NetworkingExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkingExample
+//
+//  Created by Glenna L Buford on 11.05.19.
+//  Copyright © 2019 Glenna L Buford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
